16e6692c8f0fae5fd9145d6cc99cc09d443c9a22
LordShadow-itch-io <samueloloy13@gmail.com>
2021-12-07 22:27:54 +0000

    a

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-  this.load.image('background', 'dist/playPage/assets/png/BG.BG.png');
+  this.load.image('background', 'dist/playPage/assets/png/BG/BG.svg');
 }
 
 function create ()
