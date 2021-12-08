91b2512e4259adc1d8ad52f56bfa28980503eec6
mee <95248984+yuu-nft@users.noreply.github.com>
2021-12-08 07:16:23 +0000

    Update main.js

--- main.js
+++ main.js
@@ -68,11 +68,36 @@ function create ()
 
 player.setBounce(0.2);
 player.setCollideWorldBounds(true);
+  
+  this.physics.add.collider(player, platforms);
 
 }
 
 function update ()
 {
+  if (cursors.left.isDown)
+{
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+}
+else if (cursors.right.isDown)
+{
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+}
+else
+{
+    player.setVelocityX(0);
+
+    player.anims.play('turn');
+}
+
+if (cursors.up.isDown && player.body.touching.down)
+{
+    player.setVelocityY(-330);
+}
 }
 
 /** Useful Resources  */
