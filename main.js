3b598f4edeb35426d9088b6fd630654075ddb8f7
mee <95248984+yuu-nft@users.noreply.github.com>
2021-12-08 07:08:12 +0000

    Update main.js

--- main.js
+++ main.js
@@ -41,11 +41,20 @@ var game = new Phaser.Game(config);
 function preload ()
 {
   this.load.image('sky', 'assets/BG.png');
+  this.load.image('ground', 'assets/Tiles/Tile (2).png');
 }
 
 function create ()
 {
   this.add.image(400, 300, 'sky');
+  
+  platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
