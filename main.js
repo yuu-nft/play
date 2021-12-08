7a98a0642c44b6a260977292c8078df7ee408bae
mee <95248984+yuu-nft@users.noreply.github.com>
2021-12-08 07:09:52 +0000

    Update main.js

--- main.js
+++ main.js
@@ -29,6 +29,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+  physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
