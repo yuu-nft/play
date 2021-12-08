620d927f7e594875e8a0746f8b9a64c3591844bf
mee <95248984+yuu-nft@users.noreply.github.com>
2021-12-08 06:48:20 +0000

    Update main.js

--- main.js
+++ main.js
@@ -25,6 +25,32 @@ async function logOut() {
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logOut;
 
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+  this.load.image('sky', 'assets/BG.png');
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
+
 /** Useful Resources  */
 
 // https://docs.moralis.io/moralis-server/users/crypto-login
